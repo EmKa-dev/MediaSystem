c44389c2bccb1ebb751e0f51851242b4e0fe0df4
Emil <47690578+Knubb@users.noreply.github.com>
2020-03-14 17:24:18 +0100

    Changed name of class Device to DeviceInfo.

---
+++ Communications/DeviceInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MediaSystem.Communications
+{
+    [Serializable]
+    public class DeviceInfo
+    {
+        public ConnectionInformation ConnectionInfo { get; set; }
+
+        public DataMediaType MediaType { get; set; }
+
+        public CoverInformation CoverInfo { get; set; }
+
+        public List<MediaFileInfo> MediaFiles { get; set; } = new List<MediaFileInfo>();
+    }
+}
