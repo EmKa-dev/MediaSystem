3fc494bb2e343624ae192a7ae4d84d97817fc14c
Emil <47690578+Knubb@users.noreply.github.com>
2020-04-11 23:29:07 +0200

    Added handling for toggling fullscreen.

--- MediaClientWPF/Views/VideoViewer.xaml.cs
+++ MediaClientWPF/Views/VideoViewer.xaml.cs
@@ -1,25 +1,93 @@
 ﻿using MediaSystem.DesktopClientWPF.ViewModels;
 using System;
+using System.ComponentModel;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Shell;
 
 namespace MediaSystem.DesktopClientWPF.Views
 {
     /// <summary>
     /// Interaction logic for VideoViewer.xaml
     /// </summary>
-    public partial class VideoViewer : Window
+    public partial class VideoViewer : Window, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private bool _isFullScreen;
+
+        public bool IsFullScreen
+        {
+            get { return _isFullScreen; }
+            set {
+
+                _isFullScreen = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsFullScreen)));
+            }
+        }
+
+        WindowChrome _originalChromeObject;
+        WindowResizer _windowResizer;
+
         public VideoViewer(Uri videofilepath)
         {
             InitializeComponent();
+
+            MouseDoubleClick += (o, e) => 
+            {
+                if (e.OriginalSource is MediaElement)
+                {
+                    ToggleFullSCreen();
+                }
+            };
 
             var vplayer = new VideoPlayerViewModel(videofilepath, this.MyMediaElement);
 
             this.DataContext = vplayer;
 
-            this.Loaded += (sender, args) => { vplayer.PlayCommand.Execute(sender); }; 
+            this.Loaded += (sender, args) => { vplayer.PlayCommand.Execute(sender); };
 
-            var resizer = new WindowResizer(this);
+            MaxHeight = SystemParameters.VirtualScreenHeight;
+
+            _originalChromeObject = WindowChrome.GetWindowChrome(this);
+
+            _windowResizer = new WindowResizer(this);
+        }
+
+        private void ToggleFullSCreen()
+        {
+            if (IsFullScreen)
+            {
+                _windowResizer.IncludeTaskBar = true;
+                WindowChrome.SetWindowChrome(this, _originalChromeObject);
+
+                VideoBorder.SetValue(Grid.RowProperty, 2);
+                VideoBorder.SetValue(Grid.RowSpanProperty, 1);
+                MyMediaElement.SetValue(Grid.RowSpanProperty, 1);
+                this.WindowState = WindowState.Normal;
+
+                ResizeMode = ResizeMode.CanResize;
+
+                IsFullScreen = false;
+            }
+            else
+            {
+                _windowResizer.IncludeTaskBar = false;
+                WindowChrome.SetWindowChrome(this, new WindowChrome());
+
+                VideoBorder.SetValue(Grid.RowProperty, 0);
+                VideoBorder.SetValue(Grid.RowSpanProperty, 3);
+                MyMediaElement.SetValue(Grid.RowSpanProperty, 3);
+
+                this.WindowState = WindowState.Maximized;
+
+                ResizeMode = ResizeMode.NoResize;
+
+                this.Hide();
+                this.Show();
+
+                IsFullScreen = true;            
+            }
         }
     }
 }
